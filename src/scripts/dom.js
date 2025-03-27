51cab8a55792aef068a8ed5788d23f6aa55e4e2a
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-27 11:06:18 +0100

    Create input and button

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,3 +1,16 @@
 /**
  * Responsabilidad: gestionar el los flujos de que recibe y muestra la interfáz
  * */ 
+
+const input = document.getElementById('itemInput');
+const button = document.getElementById('addItemBtn');
+const listUl = document.getElementById('itemList');
+
+button.addEventListener('click', ()=> {
+    if (input.value != "") {
+        const dynamicLi = document.createElement('li');
+        dynamicLi.textContent = input.value;
+        listUl.appendChild(dynamicLi);
+        input.value = "";
+    }
+});
