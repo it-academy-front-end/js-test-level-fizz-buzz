773b8e25da1fcc05392acdd39459edc409bda9da
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-27 12:08:58 +0100

    Add numbers in list

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,16 +1,33 @@
 /**
  * Responsabilidad: gestionar el los flujos de que recibe y muestra la interfáz
  * */ 
+//import {  } from "./events"
 
 const input = document.getElementById('itemInput');
 const button = document.getElementById('addItemBtn');
 const listUl = document.getElementById('itemList');
 
-button.addEventListener('click', ()=> {
+const historial = [[]];
+const numInput = 0;
+
+button.addEventListener('click', (numInput)=> {
     if (input.value != "") {
         const dynamicLi = document.createElement('li');
         dynamicLi.textContent = input.value;
         listUl.appendChild(dynamicLi);
         input.value = "";
+        return numInput;
     }
+    
 });
+
+
+function fizzBuzz (num) {
+    if(num % 3 === 0) {
+        historial.push([num, 'Fizz']);
+        console.log(historial);
+    }
+}
+
+fizzBuzz(numInput);
+
